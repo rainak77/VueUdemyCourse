e8452b649995f679957ea255d856d7151c246917
Md Syedul Arif <rainak_77@yahoo.com>
2023-02-14 17:21:51 +0200

    17. Understanding methods in Vue Apps

--- basics-01-starting-code/app.js
+++ basics-01-starting-code/app.js
@@ -4,6 +4,16 @@ const app=Vue.createApp({
             courseGoal: "Finnish Course and Learn Vue",
             vueLink: "https://vuejs.org"
         };
+    },
+    methods:{
+        outputGoal: function(){
+            const randomNumber= Math.random();
+            if(randomNumber < 0.5){
+                return "Learn Vue"
+            }else{
+                return "Master Vue."
+            }
+        }
     }
 });
 app.mount('#user-goal');
