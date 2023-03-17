15d58e62187030aaede5db458e75bce9762c38eb
Md Syedul Arif <rainak_77@yahoo.com>
2023-03-17 08:02:12 +0200

    Assignment 1: Time to Practice: Data Binding - added pic in  local folder

--- basics-assignment-1-problem/app.js
+++ basics-assignment-1-problem/app.js
@@ -3,7 +3,7 @@ const app = Vue.createApp({
         return {
             Name:"arif",
             Age: 41,                      
-            imageUrl: "https://commons.wikimedia.org/wiki/Category:PD_CERN-CMS#/media/File:CMS_Hcal_26_01_2007.JPG",
+            imageUrl: "pic.jpg",
         }
     },
     methods: {
