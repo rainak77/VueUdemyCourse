831767a8bd995aab09fe4a4a149e478a57bad1a8
Md Syedul Arif <rainak_77@yahoo.com>
2023-03-29 10:10:30 +0300

    chapter 25. Exploring Event Modifiers

---
+++ 25.Exploring Event Modifiers/app.js
@@ -0,0 +1,30 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 0,
+      name:'',
+      confirmedName:''
+    };
+  },
+  methods: {
+    add(num){
+      this.counter=  this.counter + num;
+    },
+    remove(num){
+      this.counter=this.counter - num;
+    },
+    setName(event,lastName){
+      this.name = event.target.value + ' ' + lastName;
+    },
+    formSubmit(event){
+     
+      alert("submit")
+    },
+    confirmedNameInput(){
+      this.confirmedName= this.name;
+    }
+    
+  },
+});
+
+app.mount('#events');
