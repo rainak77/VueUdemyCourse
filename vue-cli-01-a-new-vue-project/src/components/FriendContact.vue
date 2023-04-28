c38714966a9c52f271d831c18c48a277581955ae
Md Syedul Arif <rainak_77@yahoo.com>
2023-04-28 10:46:41 +0300

    98. Defining & Validating Custom Events

---
+++ vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,77 @@
+<template>
+    <li>
+        <h2>{{ name }} {{ isFavorite ? '(Favorite)' : '(Not Favorite)' }}</h2>
+        <button @click="togglefavorite"> toggleIsfavorite</button>
+        <button @click="toggleDetails">{{ detailsVisible ? 'Hide' : 'Show' }} Details</button>
+        <ul v-if="detailsVisible">
+            <li>name: {{ name }}</li>
+            <li>phoneNumber: {{ phoneNumber }}</li>
+            <li>email: {{ emailAddress }}</li>
+        </ul>
+    </li>
+</template>
+
+<script>
+
+
+export default {
+    // props:['name','phoneNumber','emailAddress','isFavorite'],
+    props: {
+        id: {
+            type: String,
+            required: true
+        },
+        name: {
+            type: String,
+            required: true
+        },
+        phoneNumber: {
+            type: String,
+            required: true
+        },
+        emailAddress: {
+            type: String,
+            required: true
+        },
+        isFavorite: {
+            type: Boolean,
+            required: false,
+            default: false,
+            // validator: function(value){
+            //     return  value === '1' || value === '0';
+            // }
+        }
+    },
+    emits:['toggle-favorite'],
+    // emits: {
+    //     'toggle-favorite': function (id) {
+    //         if (id) { return true; }
+    //         else {
+    //             console.warn("Id missing !!!");
+    //             return false; }
+    //     }
+    // },
+    data() {
+        return {
+            detailsVisible: false,
+            // friendIsFavorite: this.isFavorite,
+            // friend: {
+            //     id: 'manuel',
+            //     name: 'arif md',
+            //     phone: '044991222',
+            //     email: 'rainak_66@yahoo.com'
+            // }
+        }
+    },
+    methods: {
+        toggleDetails() {
+            this.detailsVisible = !this.detailsVisible;
+        },
+        togglefavorite() {
+            // this.friendIsFavorite = !this.friendIsFavorite;
+            this.$emit('toggle-favorite');
+
+        },
+    }    
+};
+</script>
